c8f4766527e48ddcd502b296241cd61ff54bf318
Black Pearl <arraa.marie@gmail.com>
2021-02-25 01:20:45 -0600

    Adding score functionality

---
+++ assets/js/questions.js
@@ -0,0 +1,44 @@
+// List of possible answers and choices
+var questions = [
+    {
+        title: "Arrays in JavaScript can be used to store ____.",
+        choices: [
+            "numbers and strings",
+            "other arrays",
+            "booleans",
+            "all of the above"],
+        answer: "all of the above"
+    },
+    {
+        title: "Commonly used data types DO NOT include:",
+        choices: [
+            "strings",
+            "booleans",
+            "alerts",
+            "numbers"],
+        answer: "alerts"
+    },
+    {
+        title: "The condition in an if / else statement is enclosed within ____.",
+        choices: [
+            "quotes",
+            "curly brackets",
+            "parentheses",
+            "square brackets"],
+        answer: "parentheses"
+    },
+    {
+        title: "A very useful tool used during development and debugging for printing content to the debugger is:",
+        choices: [
+            "JavaScript",
+            "terminal / bash",
+            "for loops",
+            "console.log"],
+        answer: "console.log"
+    },
+    {
+        title: "Which of the following is the proper tag for a page break?",
+        choices: ["<br>", "<p>", "<div>", "<meta>"],
+        answer: "<br>"
+    },
+];
---
+++ assets/js/scores.js
@@ -0,0 +1,29 @@
+// Grabs high scores
+function printHighscores() {
+    var highscores = JSON.parse(window.localStorage.getItem("highscores")) || [];
+
+    // Sorts scores from highest to lowest
+    highscores.sort(function (a, b) {
+        return b.score - a.score;
+    }):
+
+    // Creates li tag for each high score 
+    highscores.forEach(function (score) {
+        var listTag = document.createElement("li");
+        listTag.textContent = score.initials + " - " + score.score;
+
+        // Display 
+        var olEl = document.getElementById("highscores");
+        olEl.appendChild(listTag);
+    });
+}
+
+function clearHighscores() {
+    window.localStorage.removeItem("highscores");
+    window.location.reload();
+}
+
+document.getElementById("clear").onclick = clearHighscores;
+
+// Run function when page loads
+printHighscores();
